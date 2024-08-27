590a339b35968d4de49cf834ec479e53743368c1
tpsef <pseftelistheo@gmail.com>
2024-08-27 16:34:46 +0300

    Update

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -10,7 +10,6 @@ cmd="$@"
 # INFO: This script is used to wait for the MariaDB to be up and running before executing the command
 until mariadb -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
   >&2 echo "MariaDB is unavailable - sleeping"
-  echo "host: $DB_HOST", "user: $DB_USER,  password: $DB_PASSWORD"
   sleep 1
 done
 
